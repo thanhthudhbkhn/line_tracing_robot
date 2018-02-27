3539a15c5bdcf67e9a9dd3fb429c26006eb50218
thanhthudhbkhn <thanhthuk59@gmail.com>
2018-02-27 14:28:24 +0700

    return to sample code

--- sample.c
+++ sample.c
@@ -3,7 +3,7 @@
 #include <xc.h>
 #include <p18f2553.h>
 
-wait00(float k)
+wait00(short k)
 { 	 
 /*Wait time about (k?~0.01 sec.) */ 		 
     short i; 		 
@@ -14,56 +14,37 @@ wait00(float k)
 	} 		 
 } 			 
 
-int right_down(void){
-    PORTC=0x03; /* both motor on */
-    wait00(0.3); /* 0.03msec wait */
-    PORTC=0x01; /* left motor on */
-    wait00(10); /* 0.05msec wait */
-    PORTC=0x00; /* both motor off */
-    wait00(50); /* 0.2msec wait */
-}
-
-int small_right_down(void){
-    PORTC=0x03;  /* both motor on */
-    wait00(0.3); /* 0.003msec wait */
-    PORTC=0x01;  /* left motor on */
-    wait00(2);   /* 0.02msec wait */
-    PORTC=0x00;  /* both motor off */
-    wait00(50);  /* 0.20msec wait */
-}
 
 int left_down(void){
     PORTC=0x03; /* both motor on */
     wait00(0.3); /* 0.03msec wait */
-    PORTC=0x02; /* right motor on */
-    wait00(10); /* 0.05msec wait */
+    PORTC=0x01; /* right motor on */
+    wait00(5); /* 0.18msec wait */
     PORTC=0x00; /* both motor off */
-    wait00(50); /* 0.2msec wait */
+    wait00(20); /* 0.79msec wait */
 }
 
-int small_left_down(void){
-   PORTC=0x03;  /* both motor on */
-   wait00(0.3); /* 0.003msec wait */
-   PORTC=0x02;  /* right motor on */
-   wait00(2);   /* 0.02sec wait */
-   PORTC=0x00;  /* both motor off */
-   wait00(50);  /* 0.20msec wait */
+int right_down(void){
+    PORTC=0x03; /* both motor on */
+    wait00(0.3); /* 0.03msec wait */
+    PORTC=0x02; /* left motor on */
+    wait00(5); /* 0.18msec wait */
+    PORTC=0x00; /* both motor off */
+    wait00(20); /* 0.79msec wait */
 }
 
 int straight(void){
     PORTC=0x03; /* both motor on */
-    wait00(8); /* 0.08msec wait */
+    wait00(8); /* 0.20msec wait */
     PORTC=0x00; /* both motor off */
-    wait00(50); /* 0.2msec wait */
+    wait00(20); /* 0.80msec wait */
 }
 
 int led_sens(void)
 {
-PORTAbits.RA0 = PORTBbits.RB0;           
-PORTAbits.RA1 = PORTBbits.RB1;           
-PORTAbits.RA2 = PORTBbits.RB2;           
-PORTAbits.RA3 = PORTBbits.RB3;           
-PORTAbits.RA4 = PORTBbits.RB4;      
+PORTAbits.RA0 = ~PORTBbits.RB0;           
+PORTAbits.RA2 = ~PORTBbits.RB2;           
+PORTAbits.RA4 = ~PORTBbits.RB4;      
 }
 
 main(void)
@@ -77,7 +58,7 @@ main(void)
     TRISC = 0x00;            //  portC   RC all output
     
     //reset to port
-    PORTA = 0x00;           //  portA   reset output
+    PORTA = 0x00;            //  portA   reset output
     PORTB = 0x00;           //  portB    reset output
     PORTC = 0x00;           //  portC    reset output
     
@@ -90,92 +71,17 @@ main(void)
     //infinit loop
     while(1){/* infinite loop */
         led_sens();
-        switch (PORTB) {
-            case 0b00000: 
-                straight(); /* go to straight */
-                break;
-            case 0b00001: 
-                right_down();/* turn right */
-                break;
-            case 0b00010: 
-                small_right_down();/* turn a little right */
-                break;
-            case 0b00011: 
-                small_right_down();/* turn a little right */
-                break;
-            case 0b00100: 
-                straight(); /* go to straight */
-                break;
-            case 0b00101: break;
-            case 0b00110: 
-                small_right_down();/* turn a little right */
-                break;
-            case 0b00111: 
-                small_left_down();/* turn a little left */
-                break;
-            case 0b01000: 
-                small_left_down();/* turn a little left */
-                break;
-            case 0b01001: break;
-            case 0b01010: break;
-            case 0b01011: break;
-            case 0b01100: break;
-            case 0b01101: break;
-            case 0b01110: 
-                straight();
-                break;
-            case 0b01111: 
-                left_down();
-                break;
-            case 0b10000:  
-                left_down();
-                break;
-            case 0b10001:  
-                straight();
-                break;
-            case 0b10010: break;
-            case 0b10011: break;
-            case 0b10100: break;
-            case 0b10101: break;
-            case 0b10110: break;
-            case 0b10111: 
-                left_down();
-                break;
-            case 0b11000: 
-                small_left_down();/* turn a little left */
-                break;
-            case 0b11001: break;
-            case 0b11010: break;
-            case 0b11011: 
-                straight(); /* go to straight */
-                break;
-            case 0b11100: 
-                small_right_down();/* turn a little right */
-                break;
-            case 0b11101: 
-                small_right_down();/* turn a little right */
-                break;
-            case 0b11110: 
-                right_down();/* turn right */
-            case 0b11111: 
-                straight(); /* go to straight */
-                break;
-            default: 
-                straight();
-                break;
+        if(PORTBbits.RB0==1  && PORTBbits.RB2==1 && PORTBbits.RB4==0){ 
+            /*White White White White black*/
+            left_down();/* turn left */
         }
-//        small_left_down();/* turn left */
-//        if(PORTBbits.RB0==1  && PORTBbits.RB2==1 && PORTBbits.RB4==0){ 
-//            /*White White White White black*/
-//            left_down();/* turn left */
-//        }
-//        else if(PORTBbits.RB0==0 && PORTBbits.RB2==1 && PORTBbits.RB4==1){ 
-//            /*Black White White White White*/
-//            right_down(); /* turn right */
-//        }
-//        else{ /* The other case */
-//            straight(); /* go to straight */
-//        }
+        else if(PORTBbits.RB0==0 && PORTBbits.RB2==1 && PORTBbits.RB4==1){ 
+            /*Black White White White White*/
+            right_down(); /* turn right */
+        }
+        else{ /* The other case */
+            straight(); /* go to straight */
+        }
     }
             
 }
